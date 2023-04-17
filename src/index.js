c067db7bb4789ae6b672d41388a190a46df61f70
faun3 <antonioculcea@gmail.com>
2023-04-17 22:18:21 +0300

    write clearTab function and fix infinitely     creating tabs

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 import { loadPage } from "./loadPage";
 
-//loadPage();
-console.log("console woah");
+const clearTab = () => {
+  const currentTab = document.querySelector(".currentTab");
+  currentTab.replaceChildren();
+};
 
 const landingBtn = document.createElement("button");
 landingBtn.setAttribute("id", "landingBtn");
 landingBtn.textContent = "HOME";
 landingBtn.addEventListener("click", () => {
+  clearTab();
   loadPage();
 });
 
@@ -27,4 +30,10 @@ buttonsDiv.appendChild(contactBtn);
 
 const content = document.querySelector("#content");
 
+const currentTab = document.createElement("div");
+currentTab.setAttribute("class", "currentTab");
+
 content.appendChild(buttonsDiv);
+content.appendChild(currentTab);
+
+loadPage();
