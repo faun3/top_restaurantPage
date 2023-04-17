fb2b1acb612f13ab925683cebbd69202a9d1a4ff
faun3 <antonioculcea@gmail.com>
2023-04-17 22:03:28 +0300

    create buttons for tab switching

--- src/index.js
+++ src/index.js
@@ -1,4 +1,30 @@
 import { loadPage } from "./loadPage";
 
-loadPage();
+//loadPage();
 console.log("console woah");
+
+const landingBtn = document.createElement("button");
+landingBtn.setAttribute("id", "landingBtn");
+landingBtn.textContent = "HOME";
+landingBtn.addEventListener("click", () => {
+  loadPage();
+});
+
+const menuBtn = document.createElement("button");
+menuBtn.setAttribute("id", "menuBtn");
+menuBtn.textContent = "MENU";
+
+const contactBtn = document.createElement("button");
+contactBtn.setAttribute("id", "contactBtn");
+contactBtn.textContent = "CONTACT";
+
+const buttonsDiv = document.createElement("div");
+buttonsDiv.classList.add("buttonsDiv");
+
+buttonsDiv.appendChild(landingBtn);
+buttonsDiv.appendChild(menuBtn);
+buttonsDiv.appendChild(contactBtn);
+
+const content = document.querySelector("#content");
+
+content.appendChild(buttonsDiv);
